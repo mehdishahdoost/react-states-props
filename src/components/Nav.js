2279c48a05c731ed1e8b5e2f73d5122b33297b53
mehdishahdoost <mhdshahdoost@gmail.com>
2020-09-20 12:01:53 +0430

    add Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => (
+
+  <nav>
+    <li><a>Home</a></li>
+    <li><a>Personalize</a></li>
+    <li><a>Analytic</a></li>
+    <li><a>Tweets</a></li>
+  </nav>
+);
+
+export default Nav;
