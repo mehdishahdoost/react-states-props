b59c83f8e5fe7d39c7d9399d43a4ae855e9d7bc4
mehdishahdoost <mhdshahdoost@gmail.com>
2020-09-20 18:14:04 +0430

    add react state to handle counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import Nav from './components/Nav';
 import Tweets from './components/Tweets'
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+ 
+  const count = () => {
+      setCounter(counter + 1);
+  }
+
   return (
     <div className="App">
+      <h1>Props and States {counter}</h1>
       <Nav />  
       <Tweets />
+      <button onClick={count}>change Counter</button>
     </div>
   );
 }
