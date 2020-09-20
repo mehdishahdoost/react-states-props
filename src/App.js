3a7fdd216477dcd37e44c91bd2714b213a5adcef
mehdishahdoost <mhdshahdoost@gmail.com>
2020-09-20 17:09:13 +0430

    basic template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 import Nav from './components/Nav';
+import Tweets from './components/Tweets'
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
       <Nav />  
+      <Tweets />
     </div>
   );
 }
---
+++ src/components/Tweets.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tweets = () => {
+
+  return (
+    <div>
+    <h3>Tweets</h3>
+    <h4></h4>
+    </div>
+  );
+}
+
+export default Tweets;
